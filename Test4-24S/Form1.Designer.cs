facc1f543c743546d830b8de6bf3913a43e8bbf0
oMuatho <Muath04.11.21@gmail.com>
2024-05-09 15:53:47 +0300

    aa

--- Test4-24S/Form1.Designer.cs
+++ Test4-24S/Form1.Designer.cs
@@ -40,68 +40,82 @@
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 14F);
-            textBox1.Location = new Point(13, 14);
+            textBox1.Location = new Point(15, 19);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(132, 32);
+            textBox1.Size = new Size(150, 39);
             textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 14F);
-            button1.Location = new Point(29, 63);
+            button1.Location = new Point(33, 84);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(100, 42);
+            button1.Size = new Size(114, 56);
             button1.TabIndex = 2;
             button1.Text = "Add";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
             button2.Font = new Font("Segoe UI", 14F);
-            button2.Location = new Point(29, 126);
+            button2.Location = new Point(33, 168);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(100, 42);
+            button2.Size = new Size(114, 56);
             button2.TabIndex = 2;
             button2.Text = "Remove";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 14F);
-            textBox2.Location = new Point(185, 22);
+            textBox2.Location = new Point(211, 33);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(132, 32);
+            textBox2.Size = new Size(150, 39);
             textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
             textBox3.Font = new Font("Segoe UI", 14F);
-            textBox3.Location = new Point(185, 60);
+            textBox3.Location = new Point(211, 80);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(132, 32);
+            textBox3.Size = new Size(150, 39);
             textBox3.TabIndex = 1;
+            textBox3.TextChanged += textBox3_TextChanged;
             // 
             // textBox4
             // 
             textBox4.Font = new Font("Segoe UI", 14F);
-            textBox4.Location = new Point(185, 98);
+            textBox4.Location = new Point(211, 131);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(132, 32);
+            textBox4.Size = new Size(150, 39);
             textBox4.TabIndex = 1;
+            textBox4.TextChanged += textBox4_TextChanged;
             // 
             // textBox5
             // 
             textBox5.Font = new Font("Segoe UI", 14F);
-            textBox5.Location = new Point(185, 136);
+            textBox5.Location = new Point(211, 181);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(132, 32);
+            textBox5.Size = new Size(150, 39);
             textBox5.TabIndex = 1;
+            textBox5.TextChanged += textBox5_TextChanged;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(329, 191);
+            ClientSize = new Size(377, 261);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox5);
@@ -109,6 +123,7 @@
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
--- Test4-24S/Form1.cs
+++ Test4-24S/Form1.cs
@@ -1,3 +1,7 @@
+using Microsoft.VisualBasic.Devices;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Policy;
+
 namespace Test4_24S
 {
     public partial class Form1 : Form
@@ -7,5 +11,48 @@ namespace Test4_24S
             InitializeComponent();
         }
 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Enabled == true)
+                textBox1.Enabled = false;
+
+            else
+            {
+                textBox1.Enabled = true;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
